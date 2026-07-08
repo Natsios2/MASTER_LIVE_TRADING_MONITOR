75dfa1e9f83fa6f01e67bb7f6a678d32eb311565
MASTER Live Trading Monitor <live-monitor@local>
2026-07-08 20:45:16 +0200

    Update live trading dashboard

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "master-live-trading-monitor-2026-07-08-20-44-33";
+const CACHE_NAME = "master-live-trading-monitor-2026-07-08-20-45-14";
 const CORE_ASSETS = [
   "./",
   "./index.html",
